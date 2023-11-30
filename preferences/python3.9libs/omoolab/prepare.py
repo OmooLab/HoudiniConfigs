b92d9f2b74c9c745f27904e26b847b36c313a879
Ma Nan <icrdr2010@outlook.com>
2023-11-30 12:37:59 +0800

    feat: save route env to file

--- preferences/python3.9libs/omoolab/prepare.py
+++ preferences/python3.9libs/omoolab/prepare.py
@@ -8,37 +8,41 @@ HDA_PATHS = ["Contents/HDAs"]
 HDA_EXTS = [".hda", ".otl"]
 
 
-def set_project_env():
-    default_path = Path(Path.home(), "DefaultOmoos")
-
-    try:
-        default_omoos = Omoospace(default_path)
-    except:
-        default_omoos = create_omoospace(
-            name="Default Omoos",
-            root_dir=Path.home(),
-            description="Default omoospace inited by houdini as default project.",
-            reveal_in_explorer=False
-        )
-    default_path = default_omoos.root_path.as_posix()
-    hou.putenv("JOB", default_path)
-
+def set_file_env():
     try:
         file_path = Path(hou.hipFile.path()).resolve()
-        omoospace_path = Omoospace(file_path).root_path.as_posix()
-        hou.putenv("JOB", omoospace_path)
-        print(f"Current omoospace path $JOB: {omoospace_path}")
+        omoos_path = Omoospace(file_path).root_path.as_posix()
+        # hou.putenv("JOB", omoos_path)
+        hou.hscript(f"setenv JOB = {omoos_path}")
+        print(f"Current omoospace path $JOB: {omoos_path}")
 
         route_str = get_route_str(file_path)
-        hou.putenv("ROUTE", route_str)
+        # hou.putenv("ROUTE", route_str)
+        # hou.putenv not save to file. so use hscript "setenv"
+        hou.hscript(f"setenv ROUTE = {route_str}")
         print(f"Current subspace route $ROUTE: {route_str}")
 
     except:
+        try:
+            default_omoos = Omoospace(Path(Path.home(), "DefaultOmoos"))
+        except:
+            default_omoos = create_omoospace(
+                name="Default Omoos",
+                root_dir=Path.home(),
+                description="Default omoospace inited by houdini as default project.",
+                reveal_in_explorer=False
+            )
+        default_omoos_path = default_omoos.root_path.as_posix()
+        # hou.putenv("JOB", default_omoos_path)
+        hou.hscript(f"setenv JOB = {default_omoos_path}")
+        
         route_str = "Untitled"
-        hou.putenv("ROUTE", route_str)
+        # hou.putenv("ROUTE", route_str)
+        # hou.putenv not save to file. so use hscript "setenv"
+        hou.hscript(f"setenv ROUTE = {route_str}")
 
         print("No omoospace detected, so...")
-        print(f"set default omoospace path $JOB: {default_path}")
+        print(f"set default omoospace path $JOB: {default_omoos_path}")
         print(f"set default subspace route $ROUTE: {route_str}")
 
 
@@ -55,5 +59,5 @@ def install_hda():
 
 
 def init_file():
-    set_project_env()
+    set_file_env()
     install_hda()
